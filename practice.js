9357ba2ff621047b4faed25d4f09c1e5c642f477
datboi34 <edgar87250@gmail.com>
2020-07-13 16:03:06 -0400

    did javascript varibales practice using celcious and fahrenheit

--- practice.js
+++ practice.js
@@ -11,22 +11,26 @@ Remember to use descriptive variable names!
 */
 
 // 1. Store your current age into a variable.
-// Hint: let age = ...;
+// Hint:
+let age = 23;
 
 // 2. Store a maximum age into a variable.
+let oldAge = 40;
 
 // 3. Store an estimated amount per day (as a number).
 // For example 1, for 1 bag of chips per day.
+let waterBottle = 80;
 
 // 4. Calculate how many you would eat total for the rest of your life.
 // Store the result in a variable.
 // Hint: There are 365 days in a year, multiply that by the number of years and the amount per day...
-
+let consumption = 365 * waterBottle * oldAge;
 // 5. Create a variable called message describing your result.
 // "You will need NN to last you until the ripe old age of X".
+let message = "I need this many, " + consumption + " to last me until age 40.";
 
 // 6. console.log() your message.
-
+console.log(message);
 /*
 Exercise Two - Calculating your future age.
 
@@ -35,36 +39,52 @@ your age given your birth year and a year in the future.
 */
 
 // 1. Store your birth year in a variable.
+let birthYear = 1996;
 
 // 2. Store a future year in a variable.
+let worldsEnd = 2022;
 
 // 3. Calculate your 2 possible ages for that year based on the stored values.
 // For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
-
+let futureAge = worldsEnd - birthYear;
 // 4. Create a variable called message which describes the result.
 // "I will be either NN or NN in YYYY", substituting the values.
+let iAmOld = "I will be ancient or, " + futureAge + " during " + worldsEnd;
 
 // 5. console.log() your message.
-
+console.log(iAmOld);
 /*
 Exercise Three - Temperature Converter
 
 Build some code which can convert from Fahrenheit to Celcius, and also from Celcius to Fahrenheit.
 
-Based upon the method described here: https://www.mathsisfun.com/temperature-conversion.html
+// Based upon the method described here: https://www.mathsisfun.com/temperature-conversion.html
 Celcius to Fahrenheit:  Divide by 5, then multiply by 9, then add 32
 Fahrenheit to Celcius:  Deduct 32, then multiply by 5, then divide by 9
 */
 
 // 1. Store a celsius temperature into a variable.
 // Hint: let tempInCelcius = 32;
-
+let celciousTemp = 55;
 // 2. Convert it to fahrenheit and store that in a new variable.
+let newFahrenheit = (celciousTemp / 5) * 9 + 32;
 
 // 3. Output to console.log() in the format "NN°C is NN°F".
-
+console.log(
+  celciousTemp +
+    " degrees celcious is " +
+    newFahrenheit +
+    " degrees in fahrenheit"
+);
 // 4. Now store a fahrenheit temperature into a variable.
+let fahrenheitWater = 44;
 
 // 5. Convert it to celsius  and store that in a new variable.
-
+let celciousWater = ((fahrenheitWater - 32) * 5) / 9;
 // 6. Output to console.log() in the format "NN°F is NN°C."
+console.log(
+  fahrenheitWater +
+    " degrees faherenheit is " +
+    celciousWater +
+    " degrees in celcious. "
+);
